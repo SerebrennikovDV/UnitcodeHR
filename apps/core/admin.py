344403baa53cd2a2c9aea73a30882bad3491e196
Серебренников Дмитрий <kirillborovenko06@gmail.com>
2026-05-13 14:47:01 +0300

    feat(core): core app skeleton with admin branding

---
+++ apps/core/admin.py
@@ -0,0 +1,6 @@
+"""Глобальная конфигурация Django Admin для всей АС UnitcodeHR."""
+from django.contrib import admin
+
+admin.site.site_header = 'Администрирование UnitcodeHR'
+admin.site.site_title = 'UnitcodeHR'
+admin.site.index_title = 'Управление рекрутинговой системой ООО «Юниткод»'
